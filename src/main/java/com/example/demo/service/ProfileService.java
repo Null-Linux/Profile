a663e6231c9382a06e1e8af816c277550671d62c
Masaki.Kuyama <masaki.kuyama@rakus.co.jp>
2017-06-15 07:27:11 +0900

    serviceクラスのクラス名を修正． コントローラーに自己紹介ページを表示させるための機能を追加．

--- src/main/java/com/example/demo/service/ProfileService.java
+++ src/main/java/com/example/demo/service/ProfileService.java
@@ -3,14 +3,14 @@ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProfieleService {
+public class ProfileService {
 
 	public String idSelect(int id) {
 		switch (id) {
 		case 1:
 			return "hosikawa";
 		case 2:
-			return "nisioka";
+			return "nishioka";
 		case 3:
 			return "katsumata";
 		case 4:
--- src/main/java/com/example/demo/web/ProfileController.java
+++ src/main/java/com/example/demo/web/ProfileController.java
@@ -1,12 +1,18 @@
 package com.example.demo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.ProfileService;
 
 @Controller
 @RequestMapping("/")
 public class ProfileController {
 
+	@Autowired
+	ProfileService service;
+		
 	@RequestMapping("/")
 	public String index(){
 		return "index";
@@ -14,8 +20,7 @@ public class ProfileController {
 	
 	@RequestMapping("/profile")
 	public String prifile(int id){
-		
-		return "";
+		return service.idSelect(id);
 	}
 	
 }
